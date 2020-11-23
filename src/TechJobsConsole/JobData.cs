18ad8910b511e47a91d534bcbef1cce29eb9b476
Xiaowen Ao <xao@stumail.jccc.edu>
2020-11-23 13:41:32 -0600

    make search result not case senstitive

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -71,7 +71,7 @@ namespace TechJobsConsole
             {
                 string aValue = row[column];
 
-                if (aValue.Contains(value))
+                if (aValue.ToLower().Contains(value)) //case not senstitive
                 {
                     jobs.Add(row);
                 }
